7434dcb379af75254e6789a2d7138be69190f245
NathanPro2016 <blacknatan2004@gmail.com>
2024-01-30 11:04:17 +0300

    updated all v1.1

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -5,18 +5,22 @@
         <?php
         $department = mysqli_query($conn, "SELECT * FROM `department`");
         while ($rowD = mysqli_fetch_assoc($department)) {
-        ?>
+            ?>
 
             <div class="card">
-                <h5 class="text-muted"><?php echo $rowD['id'] ?></h5>
-                <h2><?php
-                    $id_dep =  $rowD['id'];
+                <h5 class="text-muted">
+                    <?php echo $rowD['id'] ?>
+                </h5>
+                <h2>
+                    <?php
+                    $id_dep = $rowD['id'];
 
                     $count = mysqli_query($conn, "SELECT COUNT(id) FROM `students` WHERE depaptment = '$id_dep'");
                     $value = mysqli_fetch_assoc($count);
                     echo $value['COUNT(id)'];
 
-                    ?></h2>
+                    ?>
+                </h2>
                 <div class="indecator">
                     <div class="inner">
                         <div id="number">
@@ -27,7 +31,7 @@
                                 echo 0;
                             } else {
 
-                                echo  $value['COUNT(id)'] / $value_all['COUNT(id)']  * 100;
+                                echo $value['COUNT(id)'] / $value_all['COUNT(id)'] * 100;
                             } ?>
                         </div>
                     </div>
@@ -42,9 +46,11 @@
                     </svg>
                 </div>
 
-                <h2><?php echo $rowD['Name'] ?></h2>
+                <h2>
+                    <?php echo $rowD['Name'] ?>
+                </h2>
             </div>
-        <?php
+            <?php
         }
         ?>
     </section>
@@ -63,9 +69,9 @@
         <?php
         if (isset($_POST["search"])) {
             $id = $_POST['id'];
-            $res_stu = mysqli_query($conn,  "SELECT * FROM `students` WHERE `id` = '$id'");
+            $res_stu = mysqli_query($conn, "SELECT * FROM `students` WHERE `id` = '$id'");
 
-        ?>
+            ?>
             <table>
                 <tr class="tr">
                     <th> ID </th>
@@ -79,15 +85,29 @@
                 </tr>
                 <?php
                 while ($rowS = mysqli_fetch_assoc($res_stu)) {
-                ?>
+                    ?>
                     <tr class="tr">
-                        <td><?php echo $rowS['id']  ?></td>
-                        <td><?php echo $rowS['firstName'] . " " . $rowS['middleName'] . " " . $rowS['lastName']; ?> </td>
-                        <td><?php echo $rowS['age']  ?></td>
-                        <td><?php echo $rowS['sex']  ?></td>
-                        <td><?php echo $rowS['email']  ?></td>
-                        <td><?php echo $rowS['grade']  ?></td>
-                        <td><?php echo $rowS['DOB']  ?></td>
+                        <td>
+                            <?php echo $rowS['id'] ?>
+                        </td>
+                        <td>
+                            <?php echo $rowS['firstName'] . " " . $rowS['middleName'] . " " . $rowS['lastName']; ?>
+                        </td>
+                        <td>
+                            <?php echo $rowS['age'] ?>
+                        </td>
+                        <td>
+                            <?php echo $rowS['sex'] ?>
+                        </td>
+                        <td>
+                            <?php echo $rowS['email'] ?>
+                        </td>
+                        <td>
+                            <?php echo $rowS['grade'] ?>
+                        </td>
+                        <td>
+                            <?php echo $rowS['DOB'] ?>
+                        </td>
 
                         <?php
                         $department = $rowS['depaptment'];
@@ -97,7 +117,8 @@
                         }
 
                         ?>
-                        <td> <a href="admin_edit_student.php?id=<?php echo $rowS['id'] ?>"> <i class="fa-solid fa-pencil"></i> <span id="edit">edit </span></a> </td>
+                        <td> <a href="admin_edit_student.php?id=<?php echo $rowS['id'] ?>"> <i class="fa-solid fa-pencil"></i>
+                                <span id="edit">edit </span></a> </td>
                         <td>
                             <form action="admin_delete_student.php" method="POST" id="confirm_form">
                                 <input type="hidden" name="id" value="<?php echo $rowS['id'] ?>">
@@ -108,65 +129,80 @@
                         <td> <a href="admin_student_result.php?id=<?php echo $rowS['id'] ?>">Resut</a> </td>
 
                     </tr>
-            </table>
+                </table>
 
-        <?php
+                <?php
                 }
-            } else {
+        } else {
 
 
 
-        ?>
-
-
-        <table>
-            <tr class="tr">
-                <th> ID </th>
-                <th> Name </th>
-                <th> Age </th>
-                <th> Sex </th>
-                <th> Email</th>
-                <th> Grade </th>
-                <th> DOB </th>
-                <th> Department </th>
-            </tr>
-
-            <?php
-
-                $res_stu = mysqli_query($conn,  "SELECT * FROM `students`, `department` WHERE students.stduent ");
-                while ($rowS = mysqli_fetch_assoc($res_stu)) {
             ?>
+
+
+            <table>
                 <tr class="tr">
-                    <td><?php echo $rowS['id']  ?></td>
-                    <td><?php echo $rowS['firstName'] . " " . $rowS['middleName'] . " " . $rowS['lastName']; ?> </td>
-                    <td><?php echo $rowS['age']  ?></td>
-                    <td><?php echo $rowS['sex']  ?></td>
-                    <td><?php echo $rowS['email']  ?></td>
-                    <td><?php echo $rowS['grade']  ?></td>
-                    <td><?php echo $rowS['DOB']  ?></td>
-
-                    <?php
-                    $department = $rowS['depaptment'];
-                    $dep = mysqli_query($conn, "SELECT * FROM `department` where id = '$department'");
-                    if ($dep_row = mysqli_fetch_assoc($dep)) {
-                        echo "<td>" . $dep_row["Name"] . "</td>";
-                    }
-
-                    ?>
-                    <td> <a href="admin_edit_student.php?id=<?php echo $rowS['id'] ?>"> <i class="fa-solid fa-pencil"></i> <span id="edit">edit </span></a> </td>
-                    <td>
-                        <form action="admin_delete_student.php" method="POST" id="confirm_form">
-                            <input type="hidden" name="id" value="<?php echo $rowS['id'] ?>">
-                            <input type="submit" value="Delete" name="delete" id="delete">
-                            <label for="delete"><i class="fa-solid fa-trash"></i></label>
-                        </form>
-                    </td>
-                    <td> <a href="admin_student_result.php?id=<?php echo $rowS['id'] ?>">Resut</a> </td>
-
+                    <th> ID </th>
+                    <th> Name </th>
+                    <th> Age </th>
+                    <th> Sex </th>
+                    <th> Email</th>
+                    <th> Grade </th>
+                    <th> DOB </th>
+                    <th> Department </th>
                 </tr>
-        <?php
+
+                <?php
+
+                $res_stu = mysqli_query($conn, "SELECT * FROM `students` ");
+                while ($rowS = mysqli_fetch_assoc($res_stu)) {
+                    ?>
+                    <tr class="tr">
+                        <td>
+                            <?php echo $rowS['id'] ?>
+                        </td>
+                        <td>
+                            <?php echo $rowS['firstName'] . " " . $rowS['middleName'] . " " . $rowS['lastName']; ?>
+                        </td>
+                        <td>
+                            <?php echo $rowS['age'] ?>
+                        </td>
+                        <td>
+                            <?php echo $rowS['sex'] ?>
+                        </td>
+                        <td>
+                            <?php echo $rowS['email'] ?>
+                        </td>
+                        <td>
+                            <?php echo $rowS['grade'] ?>
+                        </td>
+                        <td>
+                            <?php echo $rowS['DOB'] ?>
+                        </td>
+
+                        <?php
+                        $department = $rowS['depaptment'];
+                        $dep = mysqli_query($conn, "SELECT * FROM `department` where id = '$department'");
+                        if ($dep_row = mysqli_fetch_assoc($dep)) {
+                            echo "<td>" . $dep_row["Name"] . "</td>";
+                        }
+
+                        ?>
+                        <td> <a href="admin_edit_student.php?id=<?php echo $rowS['id'] ?>"> <i class="fa-solid fa-pencil"></i>
+                                <span id="edit">edit </span></a> </td>
+                        <td>
+                            <form action="admin_delete_student.php" method="POST" id="confirm_form">
+                                <input type="hidden" name="id" value="<?php echo $rowS['id'] ?>">
+                                <input type="submit" value="Delete" name="delete" id="delete">
+                                <label for="delete"><i class="fa-solid fa-trash"></i></label>
+                            </form>
+                        </td>
+                        <td> <a href="admin_student_result.php?id=<?php echo $rowS['id'] ?>">Resut</a> </td>
+
+                    </tr>
+                    <?php
                 }
-            }
+        }
 
         ?>
 
